fe1160a1866cbbcd156555baae312982bb0d6ebe
Gosha Gusev <georgiigusev@gmail.com>
2023-02-04 15:41:18 +1300

    added functions

---
+++ securechat.py
@@ -0,0 +1,19 @@
+def create_user():
+    print("1. Create a user")
+    return create_user()
+
+def login():
+    print("2. Login")
+    return  login()
+
+def connect_to_chat():
+    print("3. Connect to the chat")
+    return connect_to_chat()
+
+def list_users():
+    print("4. List all registered users")
+    return list_users()
+
+print("5. Exit")
+
+
