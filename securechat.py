ecbb3d01801f9819d5ef7c8c1cb1ec1da1717d97
Gosha Gusev <georgiigusev@gmail.com>
2023-02-06 18:35:16 +1300

    made some more program

--- securechat.py
+++ securechat.py
@@ -1,19 +1,50 @@
-def create_user():
-    print("1. Create a user")
-    return create_user()
 
-def login():
-    print("2. Login")
-    return  login()
-
-def connect_to_chat():
-    print("3. Connect to the chat")
-    return connect_to_chat()
-
-def list_users():
-    print("4. List all registered users")
-    return list_users()
-
+print("1. Create user")
+print("2. Login")
+print("3. Connect to the chat")
+print("4. List all registered users")
 print("5. Exit")
+
+number_answer = input()
+while number_answer != '5':
+    print("You have selected 'Create a user' press enter to go back.")
+    back = input()
+    if back == '':
+        print("1. Create user")
+        print("2. Login")
+        print("3. Connect to the chat")
+        print("4. List all registered users")
+        print("5. Exit")
+    elif number_answer == '2':
+        print("You have selected 'Login' press enter to go back.")
+        if back == '':
+            print("1. Create user")
+            print("2. Login")
+            print("3. Connect to the chat")
+            print("4. List all registered users")
+            print("5. Exit")
+    elif number_answer == '3':
+        print("You have selected 'Connect to the chat' press enter to go back")
+        if back == '':
+            print("1. Create user")
+            print("2. Login")
+            print("3. Connect to the chat")
+            print("4. List all registered users")
+            print("5. Exit")
+    elif number_answer == '4':
+        print("You have selected 'List all registered users' press enter to go back")
+        if back == '':
+            print("1. Create user")
+            print("2. Login")
+            print("3. Connect to the chat")
+            print("4. List all registered users")
+            print("5. Exit")
+    elif number_answer == '5':
+        print("Thank you for using our secret chat!")
+        quit()
+
+
+
+
 
 
