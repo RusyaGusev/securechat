ab099e60c9b0cc219a48c57fc17c3884b1df6fe8
Gosha Gusev <georgiigusev@gmail.com>
2023-02-07 22:45:24 +1300

    made some more of the program
    after run returns 1

--- securechat.py
+++ securechat.py
@@ -1,50 +1,30 @@
 
-print("1. Create user")
-print("2. Login")
-print("3. Connect to the chat")
-print("4. List all registered users")
-print("5. Exit")
-
-number_answer = input()
-while number_answer != '5':
-    print("You have selected 'Create a user' press enter to go back.")
-    back = input()
-    if back == '':
-        print("1. Create user")
+def main():
+    username = set()
+    print("1. Create a user")
+    print("2. Login")
+    print("3. Connect to the chat")
+    print("4. List all registered users")
+    print("5. exit")
+    number = input()
+    available_nums = {'1', '2', '3', '4', '5'}
+    while number not in available_nums:
+        print("1. Create a user")
         print("2. Login")
         print("3. Connect to the chat")
         print("4. List all registered users")
-        print("5. Exit")
-    elif number_answer == '2':
-        print("You have selected 'Login' press enter to go back.")
-        if back == '':
-            print("1. Create user")
-            print("2. Login")
-            print("3. Connect to the chat")
-            print("4. List all registered users")
-            print("5. Exit")
-    elif number_answer == '3':
-        print("You have selected 'Connect to the chat' press enter to go back")
-        if back == '':
-            print("1. Create user")
-            print("2. Login")
-            print("3. Connect to the chat")
-            print("4. List all registered users")
-            print("5. Exit")
-    elif number_answer == '4':
-        print("You have selected 'List all registered users' press enter to go back")
-        if back == '':
-            print("1. Create user")
-            print("2. Login")
-            print("3. Connect to the chat")
-            print("4. List all registered users")
-            print("5. Exit")
-    elif number_answer == '5':
-        print("Thank you for using our secret chat!")
-        quit()
+        print("5. exit")
+        while available_nums != '5':
+            def create_user():
+                print("Please input your name below: ")
+                input = input(': ')
+                while username in input:
+                    print("User already registered enter another below")
+                    input = input(': ')
 
 
-
+if __name__ == '__main__':
+    main()
 
 
 
