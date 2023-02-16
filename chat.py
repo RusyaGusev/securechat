356d15dfe41ab0ae6d7b92c37b7fcd23965720c7
Gosha Gusev <georgiigusev@gmail.com>
2023-02-16 21:57:57 +1300

    fixed some more code, still doesn't work correctly gives 3 issues

--- chat.py
+++ chat.py
@@ -1,12 +1,12 @@
 
 def main():
-    username = set()
     print("1. Create a user")
     print("2. Login")
     print("3. Connect to the chat")
     print("4. List all registered users")
     print("5. exit")
     number = input()
+    usernames = set
     available_nums = {'1', '2', '3', '4', '5'}
     while number not in available_nums:
         print("Please enter a number which corresponds to a function. ")
@@ -19,9 +19,9 @@ def main():
         number = input()
     while number != '5':
         if number == '1':
-            username_register(username)
+            username_register(usernames)
         elif number == '4':
-            list_all_user(username)
+            list_all_user(usernames)
             print("1. Create a user")
             print("2. Login")
             print("3. Connect to the chat")
@@ -32,20 +32,20 @@ def main():
     quit()
 
 
-def username_register(username):
+def username_register(usernames):
     print("Please input your name below: ")
     name = input(': ')
-    while name in username:
+    while name in usernames:
         print('Username already exists please enter another one')
         name = input(': ')
-    username.add(name)
-    print("Username " + username + " has been registered")
+    usernames.add(name)
+    print("Username " + name + " has been registered")
 
 
-def list_all_user(username):
-    for user in username:
+def list_all_user(usernames):
+    for user in usernames:
         print(user)
-
+    return list_all_user()
 
 if __name__ == '__main__':
     main()
