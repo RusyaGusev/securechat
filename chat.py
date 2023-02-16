8e6722ff922ab39d9bb9a338a5268507b3a9fad4
Gosha Gusev <georgiigusev@gmail.com>
2023-02-16 21:21:27 +1300

    more code

--- chat.py
+++ chat.py
@@ -1,12 +1,12 @@
 
 def main():
+    username = set()
     print("1. Create a user")
     print("2. Login")
     print("3. Connect to the chat")
     print("4. List all registered users")
     print("5. exit")
     number = input()
-    username = set()
     available_nums = {'1', '2', '3', '4', '5'}
     while number not in available_nums:
         print("Please enter a number which corresponds to a function. ")
@@ -17,20 +17,21 @@ def main():
         print("5. exit")
         print("Please print one of the numbers")
         number = input()
-        while number != '5':
-            if number == '1':
-                register(username)
-            elif number == '4':
-                list_user(username)
+    while number != '5':
+        if number == '1':
+            username_register(username)
+        elif number == '4':
+            list_all_user(username)
             print("1. Create a user")
             print("2. Login")
             print("3. Connect to the chat")
             print("4. List all registered users")
             print("5. exit")
             number = input()
-        if number == '5':
-            print("Thank you for using our secret chat!")
-            quit()
+    print("Thank you for using our secret chat!")
+    quit()
+
+
 def username_register(username):
     print("Please input your name below: ")
     name = input(': ')
@@ -39,13 +40,12 @@ def username_register(username):
         name = input(': ')
     username.add(name)
     print("Username " + username + " has been registered")
-    return username_register()
-register = username_register()
+
+
 def list_all_user(username):
     for user in username:
         print(user)
-    return list_all_user()
-list_user = list_all_user()
+
 
 if __name__ == '__main__':
     main()
