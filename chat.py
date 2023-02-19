2b329e741cc1d986758be811b80a401f6efb5c7c
Gosha Gusev <georgiigusev@gmail.com>
2023-02-19 14:22:03 +1300

    code works only after 2nd time

--- chat.py
+++ chat.py
@@ -6,7 +6,7 @@ def main():
     print("4. List all registered users")
     print("5. exit")
     number = input()
-    username = set
+    username = set()
     available_nums = {'1', '2', '3', '4', '5'}
     while number not in available_nums:
         print("Please enter a number which corresponds to a function. ")
@@ -20,6 +20,12 @@ def main():
     while number != '5':
         if number == '1':
             username_register(username)
+            print("1. Create a user")
+            print("2. Login")
+            print("3. Connect to the chat")
+            print("4. List all registered users")
+            print("5. exit")
+            number = input()
         elif number == '4':
             list_all_user(username)
             print("1. Create a user")
@@ -33,21 +39,25 @@ def main():
 
 
 def username_register(username):
+    print("You have selected 'Register user' press enter to go back.")
     print("Please input your name below: ")
     name = input(': ')
-    username.add(name)
-    print("Username " + name + " has been registered")
-    while name in username:
+    #print("Username " + name + " has been registered")
+    if name in username:
         print('Username already exists please enter another one')
         name = input(': ')
-    username.add(name)
-    print("Username " + name + " has been registered")
-
+        username.add(name)
+    elif name not in username:
+        username.add(name)
+        print("Username " + name + " has been registered")
+        return(username_register)
 
 def list_all_user(username):
+    print("You have selected 'list all users' press enter to go back.")
     for user in username:
-        print(user)
-    return list_all_user(username)
+        print(username)
+    return(list_all_user)
+
 
 if __name__ == '__main__':
     main()
