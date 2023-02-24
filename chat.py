05e8f51a845464caa248ab3706ba7c05cf311fd3
RusyaGusev <107546184+RusyaGusev@users.noreply.github.com>
2023-02-24 21:02:50 +1300

    added options for 2, 3. leaving username blank fix
    Added optoins for option 2, 3 responds with pick some over number. Leaving username blank also returns to menu

--- chat.py
+++ chat.py
@@ -34,6 +34,11 @@ def main():
             print("4. List all registered users")
             print("5. exit")
             number = input()
+        if number == '3' or number == '2':
+            print("Please pick one of the options except 2, 3")
+            print_menu()
+            number = input()
+
     print("Thank you for using our secret chat!")
     quit()
 
@@ -42,20 +47,33 @@ def username_register(username):
     print("You have selected 'Register user' press enter to go back.")
     print("Please input your name below: ")
     name = input(': ')
-    if name in username:
-        print('Username already exists please enter another one')
-        name = input(': ')
-        username.add(name)
-    elif name not in username:
-        username.add(name)
-        print("Username " + name + " has been registered")
-        return(username_register)
+
+    if name == "":
+        print("Please enter something and do no leave blank.")
+    else:
+        if name in username:
+            print('Username already exists please enter another one')
+            name = input(': ')
+            username.add(name)
+            print("Username " + name + " has been registered")
+        elif name not in username:
+            username.add(name)
+            print("Username " + name + " has been registered")
+            username_set = len(username)
+    return(username_register)
 
 def list_all_user(username):
     print("You have selected 'list all users' press enter to go back.")
     for user in username:
         print(username)
     return(list_all_user)
+
+def print_menu():
+    print("1. Create a user")
+    print("2. Login")
+    print("3. Connect to the chat")
+    print("4. List all registered users")
+    print("5. exit")
 
 
 if __name__ == '__main__':
