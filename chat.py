a10d9c9d5ae986a54db6efcd20e9325bcfc66566
RusyaGusev <107546184+RusyaGusev@users.noreply.github.com>
2023-04-11 17:07:47 +1200

    incoparated roles into the rest of the chat
    if selected to list all users goes into an infinite loop and roles go into an endless loop.

--- chat.py
+++ chat.py
@@ -3,6 +3,7 @@ def main():
     print_menu()
     number = input()
     username = {}
+    user_role2 = {}
     available_nums = {'1', '2', '3', '4', '5', '6'}
     while number not in available_nums:
         print("Please enter a number which corresponds to a function. ")
@@ -15,7 +16,7 @@ def main():
             print_menu()
             number = input()
         elif number == '5':
-            list_all_user(username)
+            list_all_user(username, user_role2)
             print_menu()
             number = input()
         if number == '3' or number == '2':
@@ -23,36 +24,36 @@ def main():
             print_menu()
             number = input()
         if number == '4':
-            roles()
+            roles(username, user_role2)
     print("Thank you for using our secret chat!")
     quit()
 
 
 def username_register(username):
-    print("You have selected 'Register user' press enter to go back.")
     print("Please input your name below: ")
     name = input(': ')
-    if name == "":
-        print("Please enter something and do no leave blank.")
+    if name != "":
         if name in username:
             print('Username already exists please enter another one')
             name = input(': ')
-            username.add(name)
+            username["name"] = name
+            username["role"] = 'user'
             print("Username " + name + " has been registered")
         elif name not in username:
-            username.add(name)
-
+            username["name"] = name
+            username["role"] = 'user'
             print("Username " + name + " has been registered")
-            username_set = len(username)
-    return(username_register)
+    elif name == "":
+        print("Please enter something and do no leave blank.")
+    return()
 
-user_role2 = {}
-roles_available = {'1', '2', '3'}
+
 def roles(username, user_role2):
+    roles_available = {'1', '2', '3'}
     print("Please print your username")
     role_name = input(': ')
     user_role2["name"] = role_name
-    if user_role2 == username:
+    if user_role2 != username:
         print("1. User")
         print("2. Admin")
         print("3. Moderator")
@@ -65,9 +66,8 @@ def roles(username, user_role2):
                 user_role2["name"] = role_name
                 user_role2["role"] = "admin"
                 print("You are now registered as a admin.")
-                if "admin" or "moderator" in user_role2:
-                    print("You are either already registered as a admin or you are registered "
-                          "as a moderator in which case you cannot lower your position")
+                if "admin" in user_role2:
+                    print("You are already registered as a admin you cannot lower your role.")
             if role_picked == '1':
                 print("You are already registered as a user.")
             if role_picked == '3':
@@ -75,20 +75,17 @@ def roles(username, user_role2):
                 user_role2["role"] = "moderator"
                 print("You are registered as a moderator")
                 if "moderator" in user_role2:
-                    print("You are already registered as a moderator.")
+                    print("You are already registered as a moderator you cannot lower your role.")
     else:
         print("Please enter a username which exists already or the username you used when you registered.")
-roles(username, user_role2)
-def list_all_user(username):
-    print("You have selected 'list all users' press enter to go back.")
-    for user in username:
-        print(username)
-    return(list_all_user)
+    return(roles(username, user_role2))
 
 
-
-
-
+def list_all_user(username, user_role2):
+    print("You have selected 'list all users'.")
+    for user in username and user_role2:
+        print(username and user_role2)
+    return(list_all_user(username, user_role2))
 
 
 def print_menu():
