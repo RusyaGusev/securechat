1541a342558856a558e8835a71c66f6b3b116b05
RusyaGusev <107546184+RusyaGusev@users.noreply.github.com>
2023-04-18 17:05:54 +1200

    roles now works
    list all users doesn't work and git doesn't let to commit from terminal

--- chat.py
+++ chat.py
@@ -1,9 +1,7 @@
-
-def main():
+ef main():
     print_menu()
     number = input()
-    username = {}
-    user_role2 = {}
+    users = {}
     available_nums = {'1', '2', '3', '4', '5', '6'}
     while number not in available_nums:
         print("Please enter a number which corresponds to a function. ")
@@ -12,11 +10,11 @@ def main():
         number = input()
     while number != '6':
         if number == '1':
-            username_register(username)
+            username_register(users)
             print_menu()
             number = input()
-        elif number == '5':
-            list_all_user(username, user_role2)
+        if number == '5':
+            list_all_user(users)
             print_menu()
             number = input()
         if number == '3' or number == '2':
@@ -24,68 +22,72 @@ def main():
             print_menu()
             number = input()
         if number == '4':
-            roles(username, user_role2)
+            roles(users)
+            print_menu()
+            number == input()
     print("Thank you for using our secret chat!")
     quit()
 
 
-def username_register(username):
+def username_register(users):
     print("Please input your name below: ")
     name = input(': ')
     if name != "":
-        if name in username:
+        if name in users:
             print('Username already exists please enter another one')
             name = input(': ')
-            username["name"] = name
-            username["role"] = 'user'
+            users["name"] = name
+            users["role"] = 'user'
             print("Username " + name + " has been registered")
-        elif name not in username:
-            username["name"] = name
-            username["role"] = 'user'
+        elif name not in users:
+            users["name"] = name
+            users["role"] = 'user'
             print("Username " + name + " has been registered")
     elif name == "":
         print("Please enter something and do no leave blank.")
     return()
 
 
-def roles(username, user_role2):
-    roles_available = {'1', '2', '3'}
-    print("Please print your username")
-    role_name = input(': ')
-    user_role2["name"] = role_name
-    if user_role2 != username:
-        print("1. User")
-        print("2. Admin")
-        print("3. Moderator")
-        print("Which role to a sign?")
-        role_picked = input(': ')
-        if role_picked not in roles_available:
-            print("Please write the number associated with the role.")
-        else:
-            if role_picked == '2':
-                user_role2["name"] = role_name
-                user_role2["role"] = "admin"
-                print("You are now registered as a admin.")
-                if "admin" in user_role2:
-                    print("You are already registered as a admin you cannot lower your role.")
-            if role_picked == '1':
-                print("You are already registered as a user.")
-            if role_picked == '3':
-                user_role2["name"] = role_name
-                user_role2["role"] = "moderator"
-                print("You are registered as a moderator")
-                if "moderator" in user_role2:
-                    print("You are already registered as a moderator you cannot lower your role.")
-    else:
+def roles(users):
+    print("Type E if you want to exit assigning roles, else press any button to continue assigning roles.")
+    choice = input(': ')
+    while choice != 'E':
+        roles_available = {'1', '2', '3'}
+        print("Please print your username")
+        role_name = input(': ')
+        if role_name != users:
+            print("1. User")
+            print("2. Admin")
+            print("3. Moderator")
+            print("Which role to a sign?")
+            role_picked = input(': ')
+            if role_picked not in roles_available:
+                print("Please write the number associated with the role.")
+            else:
+                if role_picked == '2':
+                    if "admin" in users:
+                        print("You are already registered as a admin, you cannot lower your role.")
+                    elif "admin" not in users:
+                        users["role: "] = "admin"
+                        print("You are now registered as a admin.")
+                if role_picked == '1':
+                    print("You are already registered as a user.")
+                if role_picked == '3':
+                    if "moderator" in users:
+                        print("You have already been registered as a moderator, you cannot lower your role.")
+                    elif "moderator" not in users:
+                        users["role: "] = "moderator"
+                        print("You are registered as a moderator")
+                choice = 'E'
         print("Please enter a username which exists already or the username you used when you registered.")
-    return(roles(username, user_role2))
+    main()
 
 
-def list_all_user(username, user_role2):
-    print("You have selected 'list all users'.")
-    for user in username and user_role2:
-        print(username and user_role2)
-    return(list_all_user(username, user_role2))
+
+def list_all_user(users):
+    for user in users:
+        print(f"Name: {user}, Role: {users[user]}")
+    return()
 
 
 def print_menu():
