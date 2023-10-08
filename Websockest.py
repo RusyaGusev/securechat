5f8f6bec02be5f2c9fcf711285f7cb1bc56a1aaf
Gosha Gusev <georgiigusev@gmail.com>
2023-10-08 20:57:56 +1300

    Messages is the one with the chat and the websocket that i followed a guide on, its makes sense but i dont know how to make the connection between the chat and server.

---
+++ Websockest.py
@@ -0,0 +1,138 @@
+
+
+import logging
+import asyncio
+import socket
+
+def main():
+    print_menu()
+    number = input()
+    users = {}
+    available_nums = {'1', '2', '3', '4', '5', '6'}
+    while number not in available_nums:
+        print("Please enter a number which corresponds to a function. ")
+        print_menu()
+        print("Please print one of the numbers")
+        number = input()
+    while number != '6':
+        if number == '1':
+            username_register(users)
+            print_menu()
+            number = input()
+        if number == '5':
+            list_all_user(users)
+            print_menu()
+            number = input()
+        if number == '3' or number == '2':
+            print("Please pick one of the options except 2, 3")
+            print_menu()
+            number = input()
+        if number == '4':
+            roles(users)
+            print_menu()
+            number = input()
+    print("Thank you for using our secret chat!")
+    quit()
+
+
+def username_register(users):
+    userinfo = {}
+    print("Please input your name below: ")
+    username = input(': ')
+    if username != "":
+        if username in users:
+            print('Username already exists please enter another one')
+            name = input(': ')
+            print("Username " + name + " has been registered")
+        elif username not in users:
+            print("Username " + username + " has been registered")
+        userinfo['role'] = 'user'
+        users[username] = userinfo
+    elif username == "":
+        print("Please enter something and do no leave blank.")
+    return()
+
+
+def roles(users):
+    new_role = {}
+    print("Type E if you want to exit assigning roles, else press enter to assign role.")
+    choice = input()
+    while choice != 'E':
+        roles_available = {'1', '2', '3'}
+        print("Please print your username")
+        role_name = input(': ')
+        if role_name in users:
+            print("1. User")
+            print("2. Admin")
+            print("3. Moderator")
+            print("Which role to a sign?")
+            role_picked = input(': ')
+            if role_picked not in roles_available:
+                print("Please write the number associated with the role.")
+            else:
+                if role_picked == '2':
+                    if 'moderator' in (users[role_name]['role']):
+                        print("You are registered as a moderator and hence you cannot lower your role.")
+                    else:
+                        if 'admin' not in users[role_name]:
+                            new_role['role'] = 'admin'
+                            print("You are now registered as a admin.")
+                            users[role_name] = new_role
+                        elif 'admin' in users[role_name]:
+                            print("You're already registered as a admin")
+                if role_picked == '1':
+                    if 'moderator' in users[role_name] or 'admin' in users[role_name]:
+                        print("You are registered as a moderator or admin hence you cannot lower your role.")
+                    else:
+                        new_role['role'] = 'user'
+                        print("You are already registered as a User.")
+                        users[role_name] = new_role
+                if role_picked == '3':
+                    if 'moderator' in users[role_name]:
+                        print("You are already registered as a moderator.")
+                    elif 'moderator' not in users[role_name]:
+                        new_role['role'] = 'moderator'
+                        print("You are registered as a moderator.")
+                        users[role_name] = new_role
+                choice = 'E'
+                print(users[role_name])
+        else:
+            print("Please enter a username with which you registered.")
+    return()
+
+d
+
+def connect(users):
+    message = {}
+    print("Please provide your ip from which your messages will be sent and recieved.")
+    ip = input(': ')
+    print("What would you like to say?")
+    message = input(': ')
+    users['ip address'] = ip
+    HOST = ip
+    PORT = 65432
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall((message))
+        data = s.recv(1024)
+    print(f"Received {data!r}")
+
+
+def list_all_user(users):
+    for user in users:
+        print(f"User: {user}, {users[user]}")
+    return()
+
+
+def print_menu():
+    print("1. Create a user")
+    print("2. Login")
+    print("3. Connect to the chat")
+    print("4. Assign role")
+    print("5. List all registered users")
+    print("6. exit")
+
+
+if __name__ == '__main__':
+    main()
+
--- fibbonacci number.py
+++
@@ -1,8 +0,0 @@
-def next_fibonacii(previous1, previous2, max_depth):
-    print(f"Current number: {previous1} {previous2}")
-    return next_fibonacii(previous2, previous1 + previous2)
-
-if __name__ == '__main__':
-    print("Please, input max depth:")
-    max_depth = input()
-    next_fibonacii(1, 1, max_depth)  # 5, 8f
