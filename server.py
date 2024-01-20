be7d4df6a210b9f070f32cfdbd392f114fcd9dec
Gosha Gusev <georgiigusev@gmail.com>
2024-01-20 21:27:24 +1300

    added websocket.recv() on line 16 so that a client can send more than one message to the server which should fix a client only being able to send one message before the server stops listening to them.

--- server.py
+++ server.py
@@ -1,46 +1,47 @@
 import websockets
 import asyncio
 
-
-async def main():
+def main():
     PORT = 7890
     print("Server listening on Port " + str(PORT))
 
     connected = set()
 
     async def echo(websocket, path):
-        print("A client just connected")
+        print("A client connected")
         connected.add(websocket)
 
         try:
-            async for message in websocket:
-                print("Received message from client: " + message)
+            while True:
+                message = await websocket.recv()
+                if message is None:
+                    break
+                print("Received message from client:", message)
+
                 for conn in connected:
                     if conn != websocket:
-                        await conn.send("Someone said:" + message + message)
+                        await conn.send("Someone said: " + message)
 
         except websockets.exceptions.ConnectionClosed as e:
-            print("A client just disconnected: " + str(e))
+            print("A client disconnected:", str(e))
         finally:
             connected.remove(websocket)
-            print("Remaining connected clients:", len(connected))
+            print("Clients remaining", len(connected))
 
             if len(connected) == 0:
-                print("No active clients, stopping server.")
+                print("No clients active, server is stopping")
                 asyncio.get_event_loop().stop()
 
-    start_server = await websockets.serve(echo, "", PORT)
+    start_server = websockets.serve(echo, "", PORT)
 
     try:
-        while True:
-            await asyncio.sleep(1)  # Add a short sleep to avoid 100% CPU usage
+        asyncio.get_event_loop().run_until_complete(start_server)
+        asyncio.get_event_loop().run_forever()
     except KeyboardInterrupt:
-        print("Stopping")
-
+        print("stopping")
 
 if __name__ == '__main__':
-    asyncio.get_event_loop().run_until_complete(main())
-    asyncio.get_event_loop().close()
+    main()
 
 
 #146.235.198.127
